7f55e6e6a2bce844ebb52ff15b00d8aa64cfd392
Robin Klaus <rmklaus12@gmail.com>
2020-04-29 12:48:59 +1000

    'Create a new project' feature complete.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -10,11 +10,24 @@ class ProjectsController < ApplicationController
   def create
     @project = Project.new(project_params)
 
-    if @project.after_save
+    if @project.save
       flash[:notice] = 'Project has been created.'
       redirect_to @project
     else
       # nothing, yet
     end
+  end
+
+  def show
+    @project = Project.find(params[:id])
+  end
+
+  private
+
+  # can call the require method on params and require the :project key exists and allows
+  # it to have :name and :description entries, any other fields submitted will be discarded
+  # Adding to private makes sure the method is not exposed
+  def project_params
+    params.require(:project).permit(:name, :description)
   end
 end
